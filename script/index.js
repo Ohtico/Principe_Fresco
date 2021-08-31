510a9771823ba00706d7a22c6a6443286b294c1f
Ohtico <viticogelvis5@gmail.com>
2021-08-31 11:24:28 -0500

    navbar

--- script/index.js
+++ script/index.js
@@ -5,12 +5,12 @@ profile.addEventListener('click', () =>{
 
     producto.innerHTML='';
     producto.innerHTML += `<div class="container card" style="width: 18rem;" id="cardProducto">
-                        <img src="${img}"
-                        class="card-img-top mt-3" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title d-flex justify-content-center">${title}</h5>
-                        <a href="#" data-id="${id}" onClick="buy()" class="btn btn-primary d-flex justify-content-center">BUY IT</a>
-                        </div></div> 
+                            <img src="${img}"
+                            class="card-img-top mt-3" alt="...">
+                            <div class="card-body">
+                            <h5 class="card-title d-flex justify-content-center">${title}</h5>
+                            <a href="#" data-id="${id}" onClick="buy()" class="btn btn-primary d-flex justify-content-center">BUY IT</a>
+                            </div></div> 
     `
 })
 
