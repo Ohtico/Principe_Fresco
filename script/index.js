1184c0309b46d9c8cc47f29366ff22ed59efb23d
Ohtico <viticogelvis5@gmail.com>
2021-08-31 11:51:52 -0500

    recorrido

--- script/index.js
+++ script/index.js
@@ -1,16 +1,25 @@
 let producto = document.getElementById('profile')
 let profile = document.getElementById('profile-tab')
+let acticulo = 'http://localhost:4000/articulos'
 
-profile.addEventListener('click', () =>{
 
-    producto.innerHTML='';
-    producto.innerHTML += `<div class="container card" style="width: 18rem;" id="cardProducto">
-                            <img src="${img}"
-                            class="card-img-top mt-3" alt="...">
-                            <div class="card-body">
-                            <h5 class="card-title d-flex justify-content-center">${title}</h5>
-                            <a href="#" data-id="${id}" onClick="buy()" class="btn btn-primary d-flex justify-content-center">BUY IT</a>
-                            </div></div> 
-    `
+
+profile.addEventListener('click', async () => {
+
+    let rest = await fetch(acticulo)
+    let data = await rest.json() 
+    console.log(data);
+
+    data.forEach(pes => {
+       const {nombre, imagenPpal, id} = pes;
+       producto.innerHTML += `<div class="container card" style="width: 18rem;" id="cardProducto">
+                               <img src="${imagenPpal}" class="card-img-top mt-3" alt="...">
+                               <div class="card-body">
+                               <h5 class="card-title d-flex justify-content-center">${nombre}</h5>
+                               <a href="#" data-id="${id}" onClick="buy()" class="btn btn-primary d-flex justify-content-center">BUY IT</a>
+                               </div></div> 
+       `
+
+    })
 })
 
