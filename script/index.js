c3901d920f29592103c30e2784677f884ee76b64
Ohtico <viticogelvis5@gmail.com>
2021-08-31 20:22:36 -0500

    canvas

--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
 let producto = document.getElementById('collecttion')
 let shop = document.getElementById('shop')
 let coleccion = document.getElementById('productos')
+let canvas = document.getElementById('offcanvasExample')
 let acticulo = 'http://localhost:4000/articulos'
 let id = 0;
 
@@ -42,63 +43,65 @@ shop.addEventListener('click', async () => {
     let imagen1 = arregloBuscado.imagen1;
     let imagen2 = arregloBuscado.imagen2;
   
-    producto.innerHTML = `
-    <div class="container">
-
-    <div class="row">
-
-      <div class="col-2">
-
-        <a href="#" onClick="change('${imagenPpal}')" class="d-flex justify-content-center">
-            <img style="width:100px" src="${imagenPpal}" alt="">
-        </a>
-
-         <a href="#" onClick="change('${imagen1}')" class="d-flex justify-content-center mt-3">
-            <img style="width:100px" src="${imagen1}" alt="">
-        </a>
-            
-        <a href="#" onClick="change('${imagen2}')" class="d-flex justify-content-center mt-3">
-            <img style="width:100px" src="${imagen2}" alt="">
-        </a>
-
-      </div>
-
-      <div class="col-5">
-
-        <img id="imageChange" style="width:80%" src="${imagenPpal}" alt="">
-
-      </div>
-
-      <div class="col-5">
-        <strong><h3 class="d-flex justify-content-left">${arregloBuscado.nombre}</h3></strong>
-        <h5 class="d-flex justify-content-left">$ ${arregloBuscado.precio}.00</h5>
-        <span class="d-flex mt-4">Size</span>
-        <div class="d-flex mt-3">
-            <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">
-            S
-            </a>
-            <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">
-            M
-            </a>
-            <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">
-            L
-            </a>
-            <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">
-            XL
-            </a>
-            <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">
-            XXL
-            </a>
-        </div>
-        <a href="#" onClick="buyNow(${id})" class="btn btn-dark d-flex justify-content-center mt-4">ADD TO CART</a>
-        <a href="#" onClick="Car(${id})" class="btn btn-primary d-flex justify-content-center mt-2">BUY IT NOW</a>
-        <span class="d-flex justify-content-left mt-2"> ${arregloBuscado.descripcion}</span>
-      </div>
-      
-    </div>`
+    producto.innerHTML = `<div class="container">
+                          <div class="row">
+                          <div class="col-2">
+                          <a href="#" onClick="change('${imagenPpal}')" class="d-flex justify-content-center">
+                          <img style="width:100px" src="${imagenPpal}" alt=""></a>
+                          <a href="#" onClick="change('${imagen1}')" class="d-flex justify-content-center mt-3">
+                          <img style="width:100px" src="${imagen1}" alt=""></a>
+                          <a href="#" onClick="change('${imagen2}')" class="d-flex justify-content-center mt-3">
+                          <img style="width:100px" src="${imagen2}" alt=""></a>
+                          </div>
+                          <div class="col-5">
+                          <img id="imageChange" style="width:80%" src="${imagenPpal}" alt=""></div>
+                          <div class="col-5">
+                          <strong><h3 class="d-flex justify-content-left">${arregloBuscado.nombre}</h3></strong>
+                          <h5 class="d-flex justify-content-left">$ ${arregloBuscado.precio}.00</h5>
+                          <span class="d-flex mt-4">Size</span>
+                          <div class="d-flex mt-3">
+                          <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">S</a>
+                          <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">M</a>
+                          <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">L</a>
+                          <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">XL</a>
+                          <a Style="text-decoration: none; color:black;" href="#" class="d-row p-3">XXL</a>
+                          </div>
+                          <a href="#" onClick="Car(${id})" class="btn btn-dark d-flex justify-content-center mt-4">ADD TO CART</a>
+                          <a href="#" onClick="buyNow(${id})" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" class="btn btn-primary d-flex justify-content-center mt-2">BUY IT NOW</a>
+                          <span class="d-flex justify-content-left mt-2"> ${arregloBuscado.descripcion}</span></div></div>`
     
 })
 
 const change = (p) => {
     document.getElementById('imageChange').setAttribute('src', p);
+}
+
+async function buyNow(id){
+
+    let rest = await fetch(acticulo);
+    let data = await rest.json()
+    let arregloBuscado = data.find(traer => traer.id == id)
+    canvas.innerHTML = '';
+    canvas.innerHTML = `<div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">${arregloBuscado.nombre}</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div>
+      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+    </div>
+    <div class="dropdown mt-3">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+        Dropdown button
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </ul>
+    </div>
+  </div>`
+    
+
+
 }
